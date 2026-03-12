a25fded5fbb8631666b48e9db59000a0eeb366ef
rajats2468 <rudradna2468@gmail.com>
2026-03-12 17:25:43 +0530

    groq AI bug fix2

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from click import prompt
 import streamlit as st
-import groq
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
